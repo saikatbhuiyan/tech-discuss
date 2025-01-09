e1cc3fbf46def3a6fc65c2e795bf38aa5c9b0a31
Saikat <saikat@Saikats-MacBook-Pro.local>
2025-01-09 22:38:41 +0600

    Project setup with nextui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -13,6 +15,7 @@ export default {
         foreground: "var(--foreground)",
       },
     },
+    darkMode: "classic",
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [nextui()],
+} satisfies Config; 

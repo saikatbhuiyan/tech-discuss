bfa052e51500d6c132b730301f7ec9c43b874b16
saikatbhuiyan <shaikotbhuiya@gmail.com>
2025-08-04 06:13:52 +0600

    Create topic components

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,3 +1,65 @@
 "use server";
+import { db } from "@/db";
+import paths from "@/paths";
+import type { Topic } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
 
-export async function createTopic() {}
+const createTopicSchema = z.object({
+  slug: z
+    .string()
+    .min(3)
+    .regex(/[a-z-]/, {
+      message: "Must be lowercase letters or dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    slug?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const result = createTopicSchema.safeParse({
+    slug: formData.get("slug"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  let topic: Topic;
+
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.slug,
+        description: result.data.description,
+      },
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        errors: {
+          _form: [error.message],
+        },
+      };
+    }
+    console.error("Error creating topic:", error);
+    return { errors: { _form: ["Failed to create topic. Please try again."] } };
+  }
+
+  revalidatePath("/");
+  redirect(paths.topicShow(topic.slug));
+}
